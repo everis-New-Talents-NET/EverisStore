9e6af8500ff396c5c44eaa677d8f1b6268770153
Your Name <you@example.com>
2021-08-23 01:07:56 -0300

    fix: add slide and jwttoken

--- EverisStore.API/Controllers/BaseController.cs
+++ EverisStore.API/Controllers/BaseController.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EverisStore.API.Controllers
 {
+    //https://github.com/dotnet/AspNetCore.Docs/blob/main/aspnetcore/security/authorization/limitingidentitybyscheme.md
+    [Authorize]
+    // [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Authorize(AuthenticationSchemes = Default)]
     [Route("api/v1/[controller]")]
     public class BaseController : ControllerBase
     {
--- EverisStore.API/Startup.cs
+++ EverisStore.API/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace EverisStore.API
@@ -26,14 +29,81 @@ namespace EverisStore.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
 
-            services.AddAutentication()
+            /* //Multiple Autenthication
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = "cookie1";
+                })
+                .AddCookie("cookie1", "cookie1", options =>
+                {
+                    options.Cookie.Name = "cookie1";
+                    options.LoginPath = "/loginc1";
+                })
+                .AddCookie("cookie2", "cookie2", options =>
+                {
+                    options.Cookie.Name = "cookie2";
+                    options.LoginPath = "/loginc2";
+                });
+                */
+            //             default:
+
+
+            /*// Code omitted for brevity
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Audience = "https://localhost:5000/";
+                    options.Authority = "https://localhost:5000/identity/";
+                })
+                .AddJwtBearer("AzureAD", options =>
+                {
+                    options.Audience = "https://localhost:5000/";
+                    options.Authority = "https://login.microsoftonline.com/eb971100-6f99-4bdc-8611-1bc8edd7f436/";
+                });*/
+
+
+            //Especifica o esquema usadao para autenticação do tipo Bearer
+            //https://developer.okta.com/blog/2018/03/23/token-authentication-aspnetcore-complete-guide
+            //https://github.com/dotnet/AspNetCore.Docs/blob/main/aspnetcore/security/authorization/limitingidentitybyscheme.md
+            //https://docs.microsoft.com/pt-br/aspnet/core/security/authorization/limitingidentitybyscheme?view=aspnetcore-5.0
+            //https://docs.microsoft.com/pt-br/aspnet/core/security/authentication/?view=aspnetcore-5.0
+            //https://sandrino.dev/blog/aspnet-core-5-jwt-authorization
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "everistore.com.br",
+                        ValidAudience = "everistore",
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SecurityKey"]))
+                    };
+
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnAuthenticationFailed = context =>
+                        {
+                            Console.WriteLine($"Token inválido...:{context.Exception.Message}");
+                            return Task.CompletedTask;
+                        },
+                        OnTokenValidated = context =>
+                        {
+                            Console.WriteLine($"Token válido...:{context.SecurityToken}");
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EverisStore.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "EverisStore.API", Version = "v1"});
             });
         }
 
@@ -51,12 +121,40 @@ namespace EverisStore.API
 
             app.UseRouting();
 
-            app.UseAuthorization();
+//registrar o modelo de autenticação no middleware
+            // app.Use(next =>
+            // {
+            //     return async ctx =>
+            //     {
+            //         switch(ctx.Request.Path)
+            //         {
+            //             case "/loginc1":
+            //                 var identity1 = new ClaimsIdentity("cookie1");
+            //                 identity1.AddClaim(new Claim("name", "Alice-c1"));
+            //                 await ctx.SignInAsync("cookie1", new ClaimsPrincipal(identity1));
+            //                 break;
+            //             case "/loginc2":
+            //                 var identity2 = new ClaimsIdentity("cookie2");
+            //                 identity2.AddClaim(new Claim("name", "Alice-c2"));
+            //                 await ctx.SignInAsync("cookie2", new ClaimsPrincipal(identity2));
+            //                 break;
+            //             case "/logoutc1":
+            //                 await ctx.SignOutAsync("cookie1");
+            //                 break;
+            //             case "/logoutc2":
+            //                 await ctx.SignOutAsync("cookie2");
+            //                 break;
+            //             default:
+            //                 await next(ctx);
+            //                 break;
+            //         }
+            //     };
+            // });
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
